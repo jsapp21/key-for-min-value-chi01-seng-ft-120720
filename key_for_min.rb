47e772f34b762c2a761c751a9e94eaf637b5fd20
James Sapp <jameseisapp@gmail.com>
2020-11-17 00:00:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,12 @@
 
 
 def key_for_min_value(name_hash)
-  
+  smallest_value = nil 
+  smallest_value_key = nil 
   name_hash.each do |key, value|
-    if value <= 1   
-      return key
+    if value < smallest_value   
+      smallest_value = value 
+      smallest_value_key = key  
     end
     
   end
